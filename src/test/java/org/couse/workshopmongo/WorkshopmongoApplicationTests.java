03eef438ace5570c7807f93e803f2e0d23dbbf94
raquel-cmps <raquelcampos@edu.unifil.br>
2023-12-29 09:15:08 -0300

    create project

---
+++ src/test/java/org/couse/workshopmongo/WorkshopmongoApplicationTests.java
@@ -0,0 +1,13 @@
+package org.couse.workshopmongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkshopmongoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
