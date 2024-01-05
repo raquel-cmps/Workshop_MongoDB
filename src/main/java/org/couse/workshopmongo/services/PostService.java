8f0c70b9da97efedf000e4c67ce27364490029b4
raquel-cmps <raquelcampos@edu.unifil.br>
2024-01-05 12:20:49 -0300

    Query with various criteria

--- src/main/java/org/couse/workshopmongo/services/PostService.java
+++ src/main/java/org/couse/workshopmongo/services/PostService.java
@@ -6,6 +6,7 @@ import org.couse.workshopmongo.services.exceptions.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.Date;
 import java.util.List;
 import java.util.Optional;
 
@@ -26,5 +27,9 @@ public class PostService {
         return repository.findByTitle(text);
     }
 
+    public List<Post> fullSearch(String text, Date minDate, Date maxDate){
+        maxDate = new Date(maxDate.getTime() + 24 * 60 * 60 * 1000);
+        return repository.fullSearch(text, minDate, maxDate);
+    }
 
 }
