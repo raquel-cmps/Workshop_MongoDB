c124833a09eb93ba152915b14c33500900a3abcd
raquel-cmps <raquelcampos@edu.unifil.br>
2024-01-05 09:53:42 -0300

    Entity Post with User nested

---
+++ src/main/java/org/couse/workshopmongo/repositories/PostRepository.java
@@ -0,0 +1,9 @@
+package org.couse.workshopmongo.repositories;
+
+import org.couse.workshopmongo.domain.Post;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PostRepository extends MongoRepository<Post, String> {
+}
--- src/main/java/org/couse/workshopmongo/services/UserService.java
+++ src/main/java/org/couse/workshopmongo/services/UserService.java
@@ -6,7 +6,6 @@ import org.couse.workshopmongo.repositories.UserRepository;
 import org.couse.workshopmongo.services.exceptions.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.List;
 import java.util.Optional;
