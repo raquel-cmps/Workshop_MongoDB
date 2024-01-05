5efbc37f65b8b493fbe1d4ef84c1948b6124b74d
raquel-cmps <raquelcampos@edu.unifil.br>
2024-01-05 10:43:21 -0300

    Add comments to post

--- src/main/java/org/couse/workshopmongo/domain/Post.java
+++ src/main/java/org/couse/workshopmongo/domain/Post.java
@@ -1,11 +1,14 @@
 package org.couse.workshopmongo.domain;
 
 import org.couse.workshopmongo.dto.AuthorDTO;
+import org.couse.workshopmongo.dto.CommentDTO;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 import java.util.Objects;
 
 @Document
@@ -17,6 +20,7 @@ public class Post implements Serializable {
     private String body;
     private AuthorDTO author;
 
+    private List<CommentDTO> comments = new ArrayList<>();
     public Post() {
     }
 
@@ -66,6 +70,10 @@ public class Post implements Serializable {
 
     public void setAuthor(AuthorDTO author) {
         this.author = author;
+    }
+
+    public List<CommentDTO> getComments() {
+        return comments;
     }
 
     @Override
