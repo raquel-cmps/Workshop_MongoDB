d81b16e0a80573ab619735109470c00bee5e7596
raquel-cmps <raquelcampos@edu.unifil.br>
2024-01-05 10:19:31 -0300

    Referencing user posts

--- src/main/java/org/couse/workshopmongo/config/Instantiation.java
+++ src/main/java/org/couse/workshopmongo/config/Instantiation.java
@@ -34,9 +34,12 @@ public class Instantiation implements CommandLineRunner {
 
         userRepository.saveAll(Arrays.asList(maria, alex, bob));
 
-        Post pos1 = new Post(null, sdf.parse("21/03/2018"), "Partiu viagem!", "Vou viajar para São Paulo. Abraços!", new AuthorDTO(maria));
+        Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu viagem!", "Vou viajar para São Paulo. Abraços!", new AuthorDTO(maria));
         Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia", "Acordei feliz hoje!", new AuthorDTO(maria));
 
-        postRepository.saveAll(Arrays.asList(pos1, post2));
+        postRepository.saveAll(Arrays.asList(post1, post2));
+
+        maria.getPosts().addAll(Arrays.asList(post1, post2));
+        userRepository.save(maria);
     }
 }
--- src/main/java/org/couse/workshopmongo/domain/User.java
+++ src/main/java/org/couse/workshopmongo/domain/User.java
@@ -1,10 +1,13 @@
 package org.couse.workshopmongo.domain;
 
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.DBRef;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.io.Serial;
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 @Document(collection = "user")
@@ -16,6 +19,8 @@ public final class User implements Serializable {
     private String id;
     private String name;
     private String email;
+    @DBRef(lazy = true) //faz referencia a outro obj - os posts so serao carregados se eu explicitamente acessa-los
+    private List<Post> posts = new ArrayList<>();
 
     public User() {
     }
@@ -48,6 +53,10 @@ public final class User implements Serializable {
 
     public void setEmail(String email) {
         this.email = email;
+    }
+
+    public List<Post> getPosts() {
+        return posts;
     }
 
     @Override
