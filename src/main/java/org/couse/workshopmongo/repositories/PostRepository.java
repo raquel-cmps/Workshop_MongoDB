647e3a1f1503c85e4e3f4bd1026c740c6203ab13
raquel-cmps <raquelcampos@edu.unifil.br>
2024-01-05 11:25:55 -0300

    Query with @Query

--- src/main/java/org/couse/workshopmongo/repositories/PostRepository.java
+++ src/main/java/org/couse/workshopmongo/repositories/PostRepository.java
@@ -2,6 +2,7 @@ package org.couse.workshopmongo.repositories;
 
 import org.couse.workshopmongo.domain.Post;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -9,4 +10,6 @@ import java.util.List;
 @Repository
 public interface PostRepository extends MongoRepository<Post, String> {
     List<Post> findByTitleContainingIgnoreCase(String title);
+    @Query("{ 'title': { $regex: ?0, $options: 'i' } }")
+    List<Post> findByTitle(String title);
 }
